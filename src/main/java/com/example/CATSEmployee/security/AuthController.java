b8abe758588f5743f9c78336ace403a2b873848f
nemtems <kipriyanof64@gmail.com>
2024-08-21 14:18:19 +0300

    Authentication endpoints were documented

--- src/main/java/com/example/CATSEmployee/security/AuthController.java
+++ src/main/java/com/example/CATSEmployee/security/AuthController.java
@@ -1,7 +1,6 @@
 package com.example.CATSEmployee.security;
 
 import com.example.CATSEmployee.security.jwt.LoginRequest;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
